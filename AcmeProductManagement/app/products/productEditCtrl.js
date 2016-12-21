615e44e37e538881738fe4841a8c4115ac4eac58
Naveen-Valmikam <naveen.valmikam@gmail.com>
2016-12-21 16:09:49 +1100

    Added form validation

--- AcmeProductManagement/app/products/productEditCtrl.js
+++ AcmeProductManagement/app/products/productEditCtrl.js
@@ -1,4 +1,7 @@
-﻿(function () {
+﻿/**
+ * Created by Deb on 8/26/2014.
+ */
+(function () {
     "use strict";
 
     angular
@@ -18,7 +21,7 @@
             vm.title = "Edit: " + vm.product.productName;
         }
         else {
-            vm.title = "New Product"
+            vm.title = "New Product";
         }
 
         vm.open = function ($event) {
@@ -28,30 +31,32 @@
             vm.opened = !vm.opened;
         };
 
-        vm.submit = function () {
-            vm.product.$save(function (data) {
-                toastr.success("Save Successful");
+        vm.submit = function (isValid) {
+            if (isValid) {
+                vm.product.$save(function (data) {
+                    toastr.success("Save Successful");
+                })
+            } else {
+                alert("Please correct the validation errors first.");
             }
-            );
-        }
+        };
 
         vm.cancel = function () {
             $state.go('productList');
-        }
+        };
 
         vm.addTags = function (tags) {
             if (tags) {
                 var array = tags.split(',');
                 vm.product.tags = vm.product.tags ? vm.product.tags.concat(array) : array;
                 vm.newTags = "";
+            } else {
+                alert("Please enter one or more tags separated by commas");
             }
-            else {
-                alert("Please enter one or more tags separated by comma");
-            }
-        }
+        };
 
-        vm.removeTags = function (idx) {
-            vm.product.tags.split(idx, 1);
-        }
+        vm.removeTag = function (idx) {
+            vm.product.tags.splice(idx, 1);
+        };
     }
 }());
